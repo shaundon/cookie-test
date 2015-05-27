e545472f0ed9b53cfc4df1aad3ebc3b0a64ed297
Shaun Donnelly <shaundonnelly@gmail.com>
2015-05-27 16:32:45 +0100

    works now

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
 
 var PORT = 7428;
+var COOKIE_NAME = "cookie-test";
 
 app.use('/', express.static('public'));
 
+app.post('/cookie', function(req, res) {
+	var domain = req.body.domain || '';
+	res.cookie(COOKIE_NAME, 'true', {
+		domain: domain,
+		httpOnly: false
+	});
+	console.log("Cookie set with " + (domain ? "domain '" + domain + "'" : "no domain."));
+	res.sendStatus(201);
+});
+
+app.delete('/cookie', function(req, res) {
+	res.clearCookie(COOKIE_NAME);
+	console.log("Cookie cleared.");
+	res.sendStatus(204);
+});
+
 app.listen(PORT, function() {
 	console.log("Listening on port " + PORT);
-})
+});
 
---
+++ public/script.js
@@ -0,0 +1,63 @@
+var Cookie = {
+	set: function() {
+		var domain = $("#cookie-domain").val();
+
+		$.ajax({
+			type: 'POST',
+			url: '/cookie',
+			data: {
+				domain: domain
+			}
+		}).done(function() {
+			Notify.show("Cookie created.", "good");
+		}).fail(function() {
+			Notify.show("Error creating cookie.", "bad");	
+		});
+	},
+
+	clear: function() {
+		$.ajax({
+			type: 'DELETE',
+			url: '/cookie'
+		}).done(function() {
+			Notify.show("Cookie cleared.", "good");
+		}).fail(function() {
+			Notify.show("Error clearing cookie.", "bad");	
+		});
+	},
+
+	get: function() {
+		var cookies = document.cookie;
+		var cookieArray = cookies.split("; ");
+		if (cookieArray.indexOf("cookie-test=true") !== -1) {
+			Notify.show("Cookie is visible.", "good");
+		}
+		else {
+			Notify.show("Cookie is not visible.", "bad");
+		}
+	}
+}
+
+var Notify = {
+	show: function(message, type) {
+		var id = Notify.generateId();
+		var html = "<div id='" + id + "' class='notification " + type + "'>" + message + "</div>";
+		$("body").append(html);
+		$("#" + id).animate({
+			"bottom": "0"
+		}, 200);
+		setTimeout(function() {
+			Notify.remove(id);
+		}, 3000);
+	},
+	generateId: function() {
+		return new Date().getTime().toString();
+	},
+	remove: function(id) {
+		$("#" + id).animate({
+			"bottom": "-300px"
+		}, 200, function() {
+			$(this).remove();
+		});
+	}
+};
